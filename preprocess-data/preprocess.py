aa7f9a1c1b82d29196b058a437b02e18679771dd
Justin Ngai <jn537@cornell.edu>
2022-05-10 14:15:31 -0400

    Finish preprocess

--- preprocess-data/preprocess.py
+++ preprocess-data/preprocess.py
@@ -1,4 +1,6 @@
+import random
 import pandas
+
 col_list = ['datesql', 'Animal_ID', 'Group_ID', 'Lactation_Num', 'DIM',
             'AnimalStatus', 'Gynecology_Status', 'Yield(gr)', 'ProdRate(gr/hr)',
             'Fat(%)', 'Avg_Fat(%)', 'Protein(%)', 'Avg_Protein(%)', 'Lactose',
@@ -11,7 +13,9 @@ col_list = ['datesql', 'Animal_ID', 'Group_ID', 'Lactation_Num', 'DIM',
 df = pandas.read_csv("../data/Milk Daily Data.csv", usecols=col_list, parse_dates=['datesql'])
 
 # Label cost estimation
-df.apply (lambda row: row["ProdRate(gr/hr)"] * row["Avg_Protein(%)"] * , axis=1)
+df['monetary_worth'] = df.apply (lambda row: row["ProdRate(gr/hr)"] * row["Avg_Protein(%)"], axis=1)
+nonzero_worth = df['monetary_worth'] > 0
+df = df[nonzero_worth]
 
 df.to_csv('processed_data.csv')
-# print(df)
+
