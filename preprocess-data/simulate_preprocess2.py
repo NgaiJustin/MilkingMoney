1910fd2c7127630d6257ed315e8de257c221d889
Justin Ngai <jn537@cornell.edu>
2022-05-10 23:58:50 -0400

    Create simulate_preprocess2.py

---
+++ preprocess-data/simulate_preprocess2.py
@@ -0,0 +1,15 @@
+import os
+import pandas
+
+src_file = 'simulate_data/original-2/daily-data.csv'
+out_dir = 'simulate_data/processed-2'
+
+# Output seperate CSV for each farm (indicated by groupID)
+df = pandas.read_csv(src_file)
+groupIDs = df["Group_ID"].unique()
+grouped = df.groupby(["Group_ID"])
+
+for groupID in groupIDs:
+    group_df = grouped.get_group(groupID)
+    out_path = os.path.join(out_dir, "farm" + str(groupID) + ".csv")
+    group_df.to_csv(out_path)
